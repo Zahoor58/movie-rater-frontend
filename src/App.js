a9a547318dfb83c8b4bc4e3ded537c13c214adc0
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-10-23 00:09:12 +0500

    fetched date from django api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [movies, setMovies] = useState(["Movie 1", "Movie 2"]);
-
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/movies/", {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token bab9a0296938f8b1e19845b146e6ee86407f2fef',
+      },
+    })
+      .then((resp) => resp.json())
+      .then((resp) => setMovies(resp))
+      .catch((error) => console.log(error))
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +23,7 @@ function App() {
       <div className="layout">
         <div>
           {movies.map((movie) => {
-            return <h1>{movie}</h1>
+            return <h1>{movie}</h1>;
           })}
         </div>
         <div>Movie Details</div>
