ad54b4e22686c7ec2eefa0298b290d9c7d059376
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-10-22 21:12:53 +0500

    fixed layouts of project

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Movie Rater</h1>
+        <h1> Movie Rater </h1>
+         
       </header>
+      <div className='layout'>
+          <div>Movie List</div>
+          <div>Movie Details</div>
+         </div>
     </div>
   );
 }
