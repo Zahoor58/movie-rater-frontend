73d7b9ad8f2b051e21b1b1cfdf4555a0e609ad16
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-10-26 01:11:03 +0500

    sent date child to parent using props and event

--- src/App.js
+++ src/App.js
@@ -2,30 +2,34 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import MovieList from "./components/movie-list";
 
-
-
 function App() {
   const [movies, setMovies] = useState(["Movie 1", "Movie 2"]);
+
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/movies/", {
       method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Token bab9a0296938f8b1e19845b146e6ee86407f2fef',
+        "Content-Type": "application/json",
+        Authorization: "Token bab9a0296938f8b1e19845b146e6ee86407f2fef",
       },
     })
       .then((resp) => resp.json())
       .then((resp) => setMovies(resp))
-      .catch((error) => console.log(error))
-  },[])
+      .catch((error) => console.log(error));
+  }, []);
+
+  const movieClicked=movie=>{
+    console.log(movie.title);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1> Movie Rater </h1>
       </header>
       <div className="layout">
-        <MovieList movies={movies}/>
-        <div>Movie Details</div>
+        <MovieList movies={movies} movieClicked={movieClicked} />
+        <div> Movie Details </div>
       </div>
     </div>
   );
--- src/components/movie-list.js
+++ src/components/movie-list.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-
-function MovieList(props){
-    return (
-        <div>
-          {props.movies && props.movies.map((movie) => {
-            return <h1>{movie.title}</h1>;
-          })}
-        </div>
-    )
+function MovieList(props) {
+  const movieClicked=movie=>evt=>{
+    props.movieClicked(movie);
+  }
+  return (
+    <div>
+      {props.movies && props.movies.map((movie,index) => {
+          return (
+            <div key={index}>
+            <h1 onClick={movieClicked(movie)}> {movie.title} </h1>
+            </div>
+          )
+        })}
+    </div>
+  );
 }
 
 export default MovieList;
