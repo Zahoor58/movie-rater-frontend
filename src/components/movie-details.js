6374cd34b67c1697e0f32b11d3f31b0910fee0e2
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-10-27 01:05:34 +0500

    dislaped movies rating

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -3,13 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 function MovieDetails(props) {
+  const mov=props.movie;
   return (
     <div>
-        {props.movie ? (
+        {mov ? (
             <div>
-                <h1>{props.movie && props.movie.title}</h1>
-                <p>{props.movie && props.movie.description}</p>
-                <FontAwesomeIcon icon={faStar} />
+                <h1>{mov && mov.title}</h1>
+                <p>{mov && mov.description}</p>
+                <FontAwesomeIcon icon={faStar} className={mov.avg_rating>0? 'orange':''} />
+                <FontAwesomeIcon icon={faStar} className={mov.avg_rating>1? 'orange':''} />
+                <FontAwesomeIcon icon={faStar} className={mov.avg_rating>2? 'orange':''} />
+                <FontAwesomeIcon icon={faStar} className={mov.avg_rating>3? 'orange':''} />
+                <FontAwesomeIcon icon={faStar} className={mov.avg_rating>4? 'orange':''} />
+                ({mov.no_of_ratings})
 
             </div>
         ): null}
