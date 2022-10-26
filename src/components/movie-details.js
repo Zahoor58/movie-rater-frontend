03f9cc848470251fc5a6818a913c44bdf07fe147
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-10-27 01:42:02 +0500

    here is error that i will fix in  next branch

--- src/components/movie-details.js
+++ src/components/movie-details.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 function MovieDetails(props) {
+   const [highlighted, setHighlighted]=useState(-1);
   const mov=props.movie;
   return (
-    <div>
+    <React.Fragment>
         {mov ? (
             <div>
                 <h1>{mov && mov.title}</h1>
@@ -16,11 +17,19 @@ function MovieDetails(props) {
                 <FontAwesomeIcon icon={faStar} className={mov.avg_rating>3? 'orange':''} />
                 <FontAwesomeIcon icon={faStar} className={mov.avg_rating>4? 'orange':''} />
                 ({mov.no_of_ratings})
-
+                <div className="rate-container">
+                  <h2>Rate it</h2>
+                  {[...Array(5)].map((e,i)=>{
+                    return <FontAwesomeIcon key={i} icon={faStar} className={highlighted>i-1? 'purple':''} 
+                    onMouseEnter={setHighlighted(i)}
+                    onMouseLeave={setHighlighted(-1)}
+                    />
+                  })}
+                </div>
             </div>
         ): null}
      
-    </div>
+    </React.Fragment>
   );
 }
 
