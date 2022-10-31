0bcef33754195b7bfa84cfdee07186a554d7bced
zahoor ahmad <86906265+Zahoor58@users.noreply.github.com>
2022-11-01 00:39:48 +0500

    layout tweaks

--- src/components/movie-form.js
+++ src/components/movie-form.js
@@ -3,7 +3,11 @@ import React from "react";
 
 function MovieForm(props){
     return(
+        <React.Fragment>
+      {props.movie ? (
         <h1>{props.movie && props.movie.title} Edit</h1>
+      ):null}
+      </React.Fragment>
     )
 }
 export default MovieForm;
